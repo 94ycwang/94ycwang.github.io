153233a2d9f2f9898426ed9fd47145e1f1d306d7
94ycwang <35606086+94ycwang@users.noreply.github.com>
2018-07-23 16:07:07 -0400

    add

--- guangdongpower/index.js
+++ guangdongpower/index.js
@@ -2,48 +2,40 @@
 //******************************************* Map HPOM output with hover-over function ******************************************
 // Set variable for map and initialize
 
-var normalm = L.tileLayer.chinaProvider('GaoDe.Normal.Map', {
-    maxZoom: 18,
-    minZoom: 5
-});
-var imgm = L.tileLayer.chinaProvider('GaoDe.Satellite.Map', {
-    maxZoom: 18,
-    minZoom: 5
-});
-var imga = L.tileLayer.chinaProvider('GaoDe.Satellite.Annotion', {
-    maxZoom: 18,
-    minZoom: 5
-});
-var normal = L.layerGroup([normalm]),
+var normalm = L.tileLayer.chinaProvider('TianDiTu.Normal.Map', {
+        maxZoom: 18,
+        minZoom: 5
+    }),
+    normala = L.tileLayer.chinaProvider('TianDiTu.Normal.Annotion', {
+        maxZoom: 18,
+        minZoom: 5
+    }),
+    imgm = L.tileLayer.chinaProvider('TianDiTu.Satellite.Map', {
+        maxZoom: 18,
+        minZoom: 5
+    }),
+    imga = L.tileLayer.chinaProvider('TianDiTu.Satellite.Annotion', {
+        maxZoom: 18,
+        minZoom: 5
+    });
+var normal = L.layerGroup([normalm, normala]),
     image = L.layerGroup([imgm, imga]);
 var baseLayers = {
     "地图": normal,
     "影像": image,
 }
-var mymap = L.map('mapid', {
+var overlayLayers = {
+}
+var map = L.map("mapid", {
     center: [20.45, 110.2],
     zoom: 11,
     layers: [normal],
     zoomControl: false
 });
-L.control.layers(baseLayers, null).addTo(mymap);
+L.control.layers(baseLayers, overlayLayers).addTo(map);
 L.control.zoom({
     zoomInTitle: '放大',
     zoomOutTitle: '缩小'
-}).addTo(mymap);
+}).addTo(map);
 
-//
-var county = JSON.parse(Get('https://94ycwang.github.io/guangdongpower/HPOM/440800.json'));	
 
-function style(feature) {
-    return {
-        fillColor: false,
-		fillOpacity:0,
-        weight: 0.3,
-        opacity:1,
-        color: 'black'
-		
-    };
-};
-
-LHPOM     = L.geoJson(county, {style: style}).addTo(mymap);
