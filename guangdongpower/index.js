df80be785042e3599beea3b74f9fffe321472bcd
94ycwang <35606086+94ycwang@users.noreply.github.com>
2018-07-23 13:47:14 -0400

    add

--- guangdongpower/index.js
+++ guangdongpower/index.js
@@ -53,7 +53,7 @@ for (var j = 0; j < HPOM.features.length; j++) {
 // Set variable for map and initialize
 var mymap =  L.map('mapid', {
     center: [20.32, 110],
-    zoom: 20,
+    zoom: 22,
 });
   mymap.createPane('radar');
 
