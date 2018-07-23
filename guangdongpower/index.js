af774704bd6ac59d96850b9ded82a586b4ec552d
94ycwang <35606086+94ycwang@users.noreply.github.com>
2018-07-23 14:38:45 -0400

    add

--- guangdongpower/index.js
+++ guangdongpower/index.js
@@ -51,10 +51,38 @@ for (var j = 0; j < HPOM.features.length; j++) {
 
 //******************************************* Map HPOM output with hover-over function ******************************************
 // Set variable for map and initialize
-var mymap =  L.map('mapid', {
+
+var normalm = L.tileLayer.chinaProvider('GaoDe.Normal.Map', {
+    maxZoom: 18,
+    minZoom: 5
+});
+var imgm = L.tileLayer.chinaProvider('GaoDe.Satellite.Map', {
+    maxZoom: 18,
+    minZoom: 5
+});
+var imga = L.tileLayer.chinaProvider('GaoDe.Satellite.Annotion', {
+    maxZoom: 18,
+    minZoom: 5
+});
+var normal = L.layerGroup([normalm]),
+    image = L.layerGroup([imgm, imga]);
+var baseLayers = {
+    "地图": normal,
+    "影像": image,
+}
+var mymap = L.map('mapid', {
     center: [20.45, 110.2],
     zoom: 11,
+    layers: [normal],
+    zoomControl: false
 });
+L.control.layers(baseLayers, null).addTo(mymap);
+L.control.zoom({
+    zoomInTitle: '放大',
+    zoomOutTitle: '缩小'
+}).addTo(mymap);
+
+
   mymap.createPane('radar');
 
   // This pane is above markers but below popups
