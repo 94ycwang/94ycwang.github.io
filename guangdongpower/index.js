ff5ed17ca19a951a08806365b935bcc4de4b3284
94ycwang <35606086+94ycwang@users.noreply.github.com>
2018-07-25 15:01:10 -0400

    add

--- guangdongpower/index.js
+++ guangdongpower/index.js
@@ -1,40 +1,39 @@
 
 //******************************************* Map HPOM output with hover-over function ******************************************
 // Set variable for map and initialize
-
 var normalm = L.tileLayer.chinaProvider('TianDiTu.Normal.Map', {
         maxZoom: 18,
-        minZoom: 5
     }),
     normala = L.tileLayer.chinaProvider('TianDiTu.Normal.Annotion', {
         maxZoom: 18,
-        minZoom: 5
     }),
     imgm = L.tileLayer.chinaProvider('TianDiTu.Satellite.Map', {
         maxZoom: 18,
-        minZoom: 5
     }),
     imga = L.tileLayer.chinaProvider('TianDiTu.Satellite.Annotion', {
         maxZoom: 18,
-        minZoom: 5
-    });
+    }),
 
+	greym = L.tileLayer.chinaProvider('Geoq.Normal.Gray', {
+        maxZoom: 18,
+    });
 
 var normal = L.layerGroup([normalm, normala]),
     image = L.layerGroup([imgm, imga]);
+
 var baseLayers = {
     "地图": normal,
     "影像": image,
-}
-var overlayLayers = {
-}
+};
+var overlayMaps = {
+};
 var map = L.map("mapid", {
     center: [20.45, 110.2],
-    zoom: 11,
+    zoom: 10,
     layers: [normal],
     zoomControl: false
 });
-L.control.layers(baseLayers, overlayLayers).addTo(map);
+L.control.layers(baseLayers,overlayMaps,{collapsed:false}).addTo(map);
 L.control.zoom({
     zoomInTitle: '放大',
     zoomOutTitle: '缩小'
@@ -43,5 +42,33 @@ L.control.zoom({
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 18, attribution: '[insert correct attribution here!]' });
 
-var clouds = L.OWM.wind({showLegend: true, opacity: 0.5, peedUnit: 'ms', appId: '50fb245848ee7d2c3bc723abd817a15a'}).addTo(map);
-var city   = L.OWM.current({intervall: 1, lang: 'de'}).addTo(map);
+// Add weather map layers
+
+var winds = L.OWM.wind({opacity: getfillOpacity(),appId: '50fb245848ee7d2c3bc723abd817a15a'});
+var city = L.OWM.current({intervall: 15,useLocalTime: false ,appId: '50fb245848ee7d2c3bc723abd817a15a'});
+var precipitation =  L.OWM.precipitationClassic({showLegend: true, opacity: getfillOpacity(), appId: '50fb245848ee7d2c3bc723abd817a15a'});
+
+
+
+function addLayerToMap(element, layer) {
+	
+    if (element.checked){
+		layer.addTo(map);
+        layer.bringToFront();		
+    } else {
+		layer.remove();					
+	};
+};
+
+// Opacity Slider
+function getfillOpacity() {
+    return $('#layeropacity').val() * '.01'
+}
+$('#layeropacity').on('input', function (value) {
+    winds.setOpacity($(this).val() * '.01');
+	precipitation.setOpacity($(this).val() * '.01');
+});
+
+//
+counties = new L.Shapefile('https://94ycwang.github.io/guangdongpower/HPOM/CHN_adm3.shp',
+	  ).addTo(map);
