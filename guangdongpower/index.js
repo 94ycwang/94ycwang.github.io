ac98093b310167588b125aafeb2734562320cb62
94ycwang <35606086+94ycwang@users.noreply.github.com>
2018-08-13 01:14:13 -0400

    Gaode Map

--- guangdongpower/index.js
+++ guangdongpower/index.js
@@ -2,12 +2,12 @@
 // Set variable for map and initialize
 
 
-var normalm = L.tileLayer.chinaProvider('TianDiTu.Normal.Map', {
+var normalm = L.tileLayer.chinaProvider('GaoDe.Normal.Map', {
         maxZoom: 18,
     }),
-    normala = L.tileLayer.chinaProvider('TianDiTu.Normal.Annotion', {
+    /*normala = L.tileLayer.chinaProvider('TianDiTu.Normal.Annotion', {
         maxZoom: 18,
-    }),
+    }),*/
     imgm = L.tileLayer.chinaProvider('TianDiTu.Satellite.Map', {
         maxZoom: 18,
     }),
@@ -19,7 +19,7 @@ var normalm = L.tileLayer.chinaProvider('TianDiTu.Normal.Map', {
         maxZoom: 18,
     });
 
-var normal = L.layerGroup([normalm, normala]),
+var normal = L.layerGroup([normalm]),
     image  = L.layerGroup([imgm, imga]);
 	
 var baseLayers = {
@@ -81,7 +81,7 @@ var precipitation =  L.OWM.precipitationClassic({showLegend: true, opacity: getf
 
 winds.setZIndex(100);
 precipitation.setZIndex(100);
-normala.setZIndex(102);
+//normala.setZIndex(102);
 imga.setZIndex(102);
 
 // Checkbox control 1
